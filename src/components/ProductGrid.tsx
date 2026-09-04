5de1e55ac6f2f61827fc55a7e307a3c907f68410
Bibidh-Raj-Shrestha <bibidhrajshrestha54321@gmail.com>
2026-09-04 21:10:44 +0545

    Feat: load more data functionality added

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -1,4 +1,4 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
 
 interface Product {
@@ -11,31 +11,39 @@ interface Product {
     discountPercentage: number;
 }
 
-export default function ProductGrid(){
+export default function ProductGrid() {
 
-    const [products,setProducts] = useState<Product[]>([]);
-    const [visibleCount, setVisibleCount] = useState(10);
+    const [products, setProducts] = useState<Product[]>([]);
+    const [visibleCount, setVisibleCount] = useState(12);
 
-    useEffect(()=>{
-            async function getProduct(){
-                const request = await fetch("http://localhost:3000/products");
-                if(!request.ok){
-                    console.log("error");
-                }
-                const data : Product[] = await request.json();
-                setProducts(data);
-                console.log(data);
+    useEffect(() => {
+        async function getProduct() {
+            const request = await fetch("http://localhost:3000/products");
+            if (!request.ok) {
+                console.log("error");
             }
-            getProduct();
-        },[]);
-    return(<>
-    <div className="grid  grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 w-full 
-                    place-contents-center mt-5">
-        {
-            products.map((product)=>(
-                <ProductCard product={product} />
-            ))
+            const data: Product[] = await request.json();
+            setProducts(data);
+            console.log(data);
         }
-    </div>
+        getProduct();
+    }, []);
+    return (<>
+        <div className="grid  grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 w-full 
+                    place-contents-center mt-5 mb-10">
+            {
+                products.slice(0, visibleCount).map((product) => (
+                    <ProductCard product={product} />
+                ))
+            }
+        </div>
+        <div className="w-full flex justify-center ">
+            <button
+                className="border p-3 text-2xl w-2xl font-semibold cursor-pointer"
+                onClick={() => setVisibleCount(prev => prev + 12)}>
+                Load More
+            </button>
+        </div>
+
     </>)
 }
